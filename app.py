3fd5c4a9d8aa541ad281129d6971cb744adcff61
Omkar463-maker <124853573+Omkar463-maker@users.noreply.github.com>
2023-05-18 22:40:02 -0700

    Added template to myapp

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import psutil
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("E:")
 
 def index():
     cpu_percent = psutil.cpu_percent()
@@ -11,7 +11,7 @@ def index():
     Message = None
     if cpu_percent > 80 or mem_percent >80:
         Message = "High CPU or Memory Utilization detected. Please add resoruces"
-    return f"CPU Utilization: {cpu_percent} and Memory Utilization: {mem_percent}"
+    return render_template("index.html", cpu_percent=cpu_percent, mem_percent=mem_percent, message=Message)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
