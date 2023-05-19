dcf48e035f8418b8f697913d61e6307f4dd6aa50
Omkar463-maker <124853573+Omkar463-maker@users.noreply.github.com>
2023-05-18 22:43:01 -0700

    Added template to myapp

--- app.py
+++ app.py
@@ -11,7 +11,6 @@ def index():
     Message = None
     if cpu_percent > 80 or mem_percent >80:
         Message = "High CPU or Memory Utilization detected. Please add resoruces"
-    return render_template("index.html", cpu_percent=cpu_percent, mem_percent=mem_percent, message=Message)
-
+    return render_template("index.html", cpu_metric=cpu_percent, mem_metric=mem_percent, message=Message)
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
